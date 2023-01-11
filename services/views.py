7e7e91ceb2e9af344267dc3b87b5c462cf880df3
abulaysov <abulaysovv@mail.ru>
2023-01-11 13:02:57 +0300

    eighth commit

--- services/views.py
+++ services/views.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.db.models import Prefetch, F, Sum
 from rest_framework import viewsets
+from django.core.cache import cache
 
 from clients.models import Client
 from services import models
@@ -22,7 +24,14 @@ class SubscriptionView(viewsets.ReadOnlyModelViewSet):
 
         response = super().list(request, *args, **kwargs)
 
+        price_cache = cache.get(settings.PRICE_CACHE_NAME)
+        if price_cache:
+            total_price = price_cache
+        else:
+            total_price = queryset.aggregate(total=Sum('price')).get('total')
+            cache.set(settings.PRICE_CACHE_NAME, total_price, 60 * 60)
+
         response_data = {'results': response.data}
-        response_data['total_amount'] = queryset.aggregate(total=Sum('price')).get('total')
+        response_data['total_amount'] = total_price
         response.data = response_data
         return response
